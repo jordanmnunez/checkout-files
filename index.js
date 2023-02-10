0cce8e80f9396ac773c2bd511e9b2a10245698dc
Jordan Matyiku Nunez <jordanm.nunez@gmail.com>
2023-02-09 18:48:48 -0700

    try

--- index.js
+++ index.js
@@ -27,25 +27,26 @@ async function getContent({ path }) {
     });
 
     if (Array.isArray(data)) {
-        console.log(`FOLDER: `, path)
         data.forEach(fileData => {
-            if (fileData.path) {
-                getContent({ path: fileData.path });
-            } else {
-                console.log(`EMPTY: `, { fileData, data })
-            }
+            getContent({ path: fileData.path });
         })
     } else {
-        console.log(`FILE: `, { path, data })
-            // try {
-            //     // let const fileContent = Buffer.from(data.content, 'base64').toString('utf-8');
-            //     saveToFile({
-            //         path,
-            //         fileString: await getRawFile(data),
-            //     });
-            // } catch (e) {
-            //     throw e
-            // }
+        let fileString = Buffer.from(data.content, 'base64').toString('utf-8');
+        if (fileString.length == 0 && data.size > 0) {
+            data = getRawFile({ path });
+            console.log(`RAW `, data);
+        }
+
+        saveToFile({ path, fileString });
+        // try {
+        //     // let const fileContent = Buffer.from(data.content, 'base64').toString('utf-8');
+        //     saveToFile({
+        //         path,
+        //         fileString: await getRawFile(data),
+        //     });
+        // } catch (e) {
+        //     throw e
+        // }
 
     }
 }
